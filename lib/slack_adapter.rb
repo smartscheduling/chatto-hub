425025da5610e7976d0ee44e6b9fe2c50b12ffa8
Spencer Dixon <spencercdixon@gmail.com>
2015-07-30 14:00:06 -0400

    Added tests for SlackAdapter

--- lib/slack_adapter.rb
+++ lib/slack_adapter.rb
@@ -1,22 +1,22 @@
 require 'rest-client'
-
+# Configuration in config/initializers/slack
 class SlackAdapter
   class SlackTeamInviteError < StandardError; end
   DEFAULT_CHANNEL_ID="C067NNHEY"
 
-  attr_reader :team_id
-  def initialize(team_id=nil)
-    @team_id = team_id || ENV['SLACK_TEAM_ID']
+  cattr_accessor :team_id, :token, :team_name
+
+  def slack
+    Slack
   end
 
   def channels_create(opts={})
-    Slack.channels_create(opts)
+    slack.channels_create(opts)
   end
 
   def send_team_invite(email, channel_id=DEFAULT_CHANNEL_ID)
     time = Time.now.to_i
-    url = "https://#{ENV['SLACK_TEAM_NAME']}.slack.com/api/users.admin.invite?t=#{time}"
-    token = ENV['SLACK_TEST_TOKEN']
+    url = "https://#{team_name}.slack.com/api/users.admin.invite?t=#{time}"
 
     query = { token: token, team_id: team_id, email: email,
       set_active: true, _attempts: 1, channels: channel_id}.to_query
@@ -24,14 +24,14 @@ class SlackAdapter
     response = RestClient::Request.execute(method: :post, url: url, payload: query)
     response = JSON.parse(response)
 
-    unless response["ok"] || response["error"] == "already_in_team"
+    if !response["ok"] || response["error"] == "already_in_team"
       raise SlackTeamInviteError
     end
   end
 
   def send_channel_invite(channel_id, user_id)
-    response = Slack.channels_invite(channel: channel_id, user: user_id)
-    unless response["ok"] || response["error"] == "already_in_channel"
+    response = slack.channels_invite(channel: channel_id, user: user_id)
+    if !response["ok"] || response["error"] == "already_in_channel"
       raise SlackTeamInviteError
     end
     true
@@ -39,13 +39,12 @@ class SlackAdapter
 
   def channel_url(name)
     name = sanitize_channel_name(name)
-    "https://#{ENV['SLACK_TEAM_NAME']}.slack.com/messages/#{name}/"
+    "https://#{team_name}.slack.com/messages/#{name}/"
   end
 
-  # move id to project model
   def find_channel_by_name(name)
     name = sanitize_channel_name(name)
-    channels = Slack.channels_list
+    channels = slack.channels_list
     channels["channels"].find { |c| c["name"] == name }
   end
 
@@ -58,13 +57,13 @@ class SlackAdapter
   end
 
   def user_on_team?(email)
-    Slack.users_list["members"].any? do |user|
+    slack.users_list["members"].any? do |user|
       user["profile"]["email"] == email
     end
   end
 
   def get_user_id_by_email(email)
-    user = Slack.users_list["members"].find do |user|
+    user = slack.users_list["members"].find do |user|
       user["profile"]["email"] == email
     end
 
---
+++ spec/lib/slack_adapter_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+describe SlackAdapter do
+  subject { SlackAdapter.new }
+
+  describe "#channels_create" do
+    it "is a thin wrapper around Slack Gem's method" do
+      slack_double = double(channels_create: true)
+      stub_slack!(slack_double)
+      expect(slack_double).to receive(:channels_create)
+      subject.channels_create
+    end
+  end
+
+  describe "#send_team_invite" do
+    before do
+      allow(Time).to receive(:now).and_return(42)
+    end
+
+    it "executes a post with the right queries" do
+      email = "some_email"
+      payload = "_attempts=1&channels=C067NNHEY&email=#{email}&set_active=true&team_id=#{ENV['SLACK_TEAM_ID']}&token=#{ENV['SLACK_TEST_TOKEN']}"
+      proper_args = { method: :post, url: "https://datamarathontest.slack.com/api/users.admin.invite?t=42", payload: payload }
+      expect(RestClient::Request).to receive(:execute).with(proper_args).and_return({ok: true}.to_json)
+      subject.send_team_invite(email)
+    end
+
+    it "returns proper error if applicable" do
+      allow(RestClient::Request).to receive(:execute).and_return("{\"error\":\"already_in_team\"}")
+      expect{subject.send_team_invite("email")}.to raise_error(SlackAdapter::SlackTeamInviteError)
+    end
+  end
+
+  describe "#send_channel_invite" do
+    it "is a thin wrapper around Slack Gem's method" do
+      slack_double = double(channels_invite: { "ok" => true })
+      stub_slack!(slack_double)
+      expect(slack_double).to receive(:channels_invite)
+      subject.send_channel_invite('channel_id', 'user_id')
+    end
+
+    it "returns true if successful" do
+      slack_double = double(channels_invite: { "ok" => true })
+      stub_slack!(slack_double)
+      expect(subject.send_channel_invite('channel_id', 'user_id')).to eq true
+    end
+
+    it "raises an error if user is already in channel" do
+      slack_double = double(channels_invite: { "ok" => false })
+      stub_slack!(slack_double)
+      expect{subject.send_channel_invite('channel_id', 'user_id')}.to raise_error(SlackAdapter::SlackTeamInviteError)
+    end
+  end
+
+  describe "#channel_url" do
+    it "returns url of a name sanitized for a channel" do
+      name = "sweet channel"
+      expect(subject.channel_url(name)).to eq "https://#{ENV['SLACK_TEAM_NAME']}.slack.com/messages/sweet-channel/"
+    end
+  end
+
+  describe "#find_channel_by_name" do
+    it "returns the channel if found" do
+      channel_list = { "channels" => [ { "name" => "found!"} ] }
+      slack_double = double(channels_list: channel_list)
+      stub_slack!(slack_double)
+      expect(subject.find_channel_by_name("found!")).to eq({ "name" => "found!" })
+    end
+  end
+
+  describe "#find_channel_id_by_name" do
+    it "returns the id of the found channel" do
+      channel_list = { "channels" => [ { "name" => "found!", "id" => 42} ] }
+      slack_double = double(channels_list: channel_list)
+      stub_slack!(slack_double)
+      expect(subject.find_channel_id_by_name("found!")).to eq(42)
+    end
+  end
+
+  describe "#sanitize_channel_name" do
+    it "removes empty spaces and replaces with dashes" do
+      name = "this is a name"
+      expect(subject.sanitize_channel_name(name)).to eq "this-is-a-name"
+    end
+  end
+
+  describe "#user_on_team?" do
+    before do
+      user_list = { "members" => [{ "profile" => { "email" => "example@email.com" }} ] }
+      slack_double = double(users_list: user_list)
+      stub_slack!(slack_double)
+    end
+
+    it "returns true when user is on team" do
+      expect(subject.user_on_team?("example@email.com")).to eq true
+    end
+
+    it "returns false when not" do
+      expect(subject.user_on_team?("not_valid_email")).to eq false
+    end
+  end
+
+  describe "#get_user_id_by_email" do
+    it "returns the users id that it finds" do
+      user_list = { "members" => [{ "id" => 42, "profile" => { "email" => "example@email.com" }} ] }
+      slack_double = double(users_list: user_list)
+      stub_slack!(slack_double)
+      expect(subject.get_user_id_by_email("example@email.com")).to eq 42
+    end
+  end
+end
+
+def stub_slack!(double)
+  allow_any_instance_of(SlackAdapter).to receive(:slack).and_return(double)
+end
