b6967d90c391608dd55a66b0d24a25d6cc9eeb92
Spencer Dixon <spencercdixon@gmail.com>
2015-08-10 11:00:48 -0400

    Removed bourbon and foundation remnants.  Added styling that chris wanted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
-//= require_tree .
+//= require materialize-sprockets
 
-// foundation
-$(function(){ $(document).foundation(); });
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal({});
+
+  $('.tooltipped').tooltip({delay: 50});
+});
 
